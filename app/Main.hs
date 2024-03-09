9145497b6239fa3ca6db2e8105fd87a16b7c4eb8
Adam Zadrożny <zadroznyadam@pm.me>
2024-03-09 13:19:46 +0100

    Respond with content

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,10 @@ module Main (main) where
 import Control.Exception (bracket)
 import Control.Monad (forM_, forever)
 import Data.ByteString.Char8 qualified as BC
+import Data.Maybe (fromMaybe)
 import Network.Socket
 import Network.Socket.ByteString
 import System.IO (BufferMode (..), hSetBuffering, stdout)
-
-status200 :: BC.ByteString
-status200 = "HTTP/1.1 200 OK\r\n\r\n"
-
-status404 :: BC.ByteString
-status404 = "HTTP/1.1 404 Not Found\r\n\r\n"
 
 main :: IO ()
 main = do
@@ -41,24 +36,50 @@ main = do
     listen serverSocket 5
 
     -- Accept connections and handle them forever
-    forever
-        $ bracket (accept serverSocket) (\(clientSocket, _) -> close clientSocket)
-        $ \(clientSocket, clientAddr) -> do
-            BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
+    forever $ do
+        bracket
+            (accept serverSocket)
+            (\(clientSocket, _) -> close clientSocket)
+            handleConnection
 
-            body <- recv clientSocket 4096
-            let contentLines = BC.strip <$> BC.lines body
-            BC.putStrLn ""
-            forM_ contentLines BC.putStrLn
+handleConnection :: (Socket, SockAddr) -> IO ()
+handleConnection (clientSocket, clientAddr) = do
+    BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
 
-            _ <- case BC.words <$> contentLines of
-                ("GET" : "/" : _) : _ -> do
-                    BC.putStrLn "path: /"
-                    sendAll clientSocket status200
-                ("GET" : path : _) : _ -> do
-                    BC.putStrLn $ "path: " <> path
-                    sendAll clientSocket status404
-                _ ->
-                    sendAll clientSocket status404
+    body <- recv clientSocket 4096
+    let contentLines = BC.strip <$> BC.lines body
+    BC.putStrLn ""
+    forM_ contentLines BC.putStrLn
 
-            close clientSocket
+    let response = case BC.words <$> contentLines of
+            ("GET" : "/" : _) : _ -> do
+                mkResponse Status200 [] ""
+            ("GET" : path : _) : _ ->
+                if "/echo/" `BC.isPrefixOf` path
+                    then echoResponse (BC.drop 6 path)
+                    else mkResponse Status404 [] ""
+            _ ->
+                mkResponse Status404 [] ""
+
+    sendAll clientSocket response
+
+data StatusCode
+    = Status200
+    | Status404
+
+toByteString :: StatusCode -> BC.ByteString
+toByteString Status200 = "200 OK"
+toByteString Status404 = "404 Not Found"
+
+mkResponse :: StatusCode -> [BC.ByteString] -> BC.ByteString -> BC.ByteString
+mkResponse status headers body =
+    "HTTP/1.1 " <> toByteString status <> "\r\n" <> BC.intercalate "\r\n" headers <> "\r\n\r\n" <> body
+
+echoResponse :: BC.ByteString -> BC.ByteString
+echoResponse msg =
+    mkResponse
+        Status200
+        [ "Content-Type: text/plain"
+        , "Content-Length: " <> (BC.pack . show . BC.length) msg
+        ]
+        msg
